72d4677160664c6cfa4baeda548f61111b69390b
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 15:43:59 +0530

    comments removed

--- GetPairNumberFromColor.cs
+++ GetPairNumberFromColor.cs
@@ -9,22 +9,16 @@ namespace TelCo.ColorCoder
     /// <returns></returns>
     private static int GetPairNumberFromColor(ColorPair pair)
     {
-        // Find the major color in the array and get the index
         int majorIndex = -1;
-        majorIndex = Array.IndexOf(colorMapMajor, pair.majorColor);
-        
-        // Find the minor color in the array and get the index
+        majorIndex = Array.IndexOf(colorMapMajor, pair.majorColor);        
         int minorIndex = -1;
         minorIndex = Array.IndexOf(colorMapMinor, pair.minorColor);
         
-        // If colors can not be found throw an exception
         if (majorIndex == -1 || minorIndex == -1)
         {
             throw new ArgumentException(
                 string.Format("Unknown Colors: {0}", pair.ToString()));
         }
-
-        // Compute pair number and Return  
         // (Note: +1 in compute is because pair number is 1 based, not zero)
         return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
     }
