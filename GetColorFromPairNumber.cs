26d324377cd129f46db80daa092adbdc78b3f9bd
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 15:44:57 +0530

    comments removed

--- GetColorFromPairNumber.cs
+++ GetColorFromPairNumber.cs
@@ -16,18 +16,13 @@ namespace TelCo.ColorCoder
         {
             throw new ArgumentOutOfRangeException(
                 string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-        }
-        
-        // Find index of major and minor color from pair number
+        }        
         int zeroBasedPairNumber = pairNumber - 1;
         int majorIndex = zeroBasedPairNumber / minorSize;
         int minorIndex = zeroBasedPairNumber % minorSize;
 
-        // Construct the return val from the arrays
         ColorPair pair = new ColorPair() { majorColor = colorMapMajor[majorIndex],
             minorColor = colorMapMinor[minorIndex] };
-        
-        // return the value
         return pair;
     }
   }
