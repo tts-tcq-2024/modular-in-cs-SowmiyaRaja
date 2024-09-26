790fa89c828e4f5aed1403a86dae851d2efc9077
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 15:45:50 +0530

    removed returns

--- GetColorFromPairNumber.cs
+++ GetColorFromPairNumber.cs
@@ -6,7 +6,6 @@ namespace TelCo.ColorCoder
     /// Given a pair number function returns the major and minor colors in that order
     /// </summary>
     /// <param name="pairNumber">Pair number of the color to be fetched</param>
-    /// <returns></returns>
     private static ColorPair GetColorFromPairNumber(int pairNumber)
     {
         // The function supports only 1 based index. Pair numbers valid are from 1 to 25
