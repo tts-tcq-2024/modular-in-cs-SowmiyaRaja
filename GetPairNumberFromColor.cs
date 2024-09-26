cc2b3347c9c00ab1087ca734f89752c2189ef19c
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 15:45:34 +0530

    removed returns

--- GetPairNumberFromColor.cs
+++ GetPairNumberFromColor.cs
@@ -6,7 +6,6 @@ namespace TelCo.ColorCoder
     /// Given the two colors the function returns the pair number corresponding to them
     /// </summary>
     /// <param name="pair">Color pair with major and minor color</param>
-    /// <returns></returns>
     private static int GetPairNumberFromColor(ColorPair pair)
     {
         int majorIndex = -1;
