60e870bedcfd3c248311cba96ac63ff63faadad2
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 16:29:22 +0530

    Update MappingFromColorNameToColorCode.cs

--- MappingFromColorNameToColorCode.cs
+++ MappingFromColorNameToColorCode.cs
@@ -8,7 +8,15 @@ namespace TelCo.ColorCoder
   {
     private static void MappingFromColorNameToColorCode()
     {
-      
+      int colorCode = 1;
+      Console.WriteLine(String.Format("|{0}\t|{1}\t|{2}|", "Major Color", "Minor Color", "Pair No"));
+      foreach(var majorColor in colorMapMajor)
+      {
+        foreach(var minorColor in colorMapMinor)
+        {
+          
+        }
+      }
     }
   }
 }
