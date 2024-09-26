aa7f6a476bcf3ff72bc813b860a3242efdcf10b4
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-26 13:28:37 +0530

    modified access specifier of static class

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// Static constructor required to initialize static variable
         /// </summary>
-        static Program()
+        public static Program()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
