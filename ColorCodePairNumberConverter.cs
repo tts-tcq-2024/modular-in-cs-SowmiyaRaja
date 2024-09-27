03d17b5a580a77b9f753037292c9f98a7c9d256a
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-09-27 11:46:21 +0530

    Update and rename GetColorFromPairNumber.cs to ColorCodePairNumberConverter.cs

--- ColorCodePairNumberConverter.cs
+++ ColorCodePairNumberConverter.cs
@@ -24,5 +24,24 @@ namespace TelCo.ColorCoder
             minorColor = colorMapMinor[minorIndex] };
         return pair;
     }
+    /// <summary>
+    /// Given the two colors the function returns the pair number corresponding to them
+    /// </summary>
+    /// <param name="pair">Color pair with major and minor color</param>
+    private static int GetPairNumberFromColor(ColorPair pair)
+    {
+        int majorIndex = -1;
+        majorIndex = Array.IndexOf(colorMapMajor, pair.majorColor);        
+        int minorIndex = -1;
+        minorIndex = Array.IndexOf(colorMapMinor, pair.minorColor);
+        
+        if (majorIndex == -1 || minorIndex == -1)
+        {
+            throw new ArgumentException(
+                string.Format("Unknown Colors: {0}", pair.ToString()));
+        }
+        // (Note: +1 in compute is because pair number is 1 based, not zero)
+        return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+    }
   }
 }
